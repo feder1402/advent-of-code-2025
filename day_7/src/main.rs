4c7e11e2693507525e4a49efa8cd88e53b9814e9
feder1402 <feder1402@users.noreply.github.com>
2025-12-13 15:08:26 -0800

    More changes

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,102 +1,110 @@
+use std::fs;
+
+#[allow(unused)]
 fn main() {
-    // Read the input
-    let input = include_str!("../sample_input.txt");
-    let mut lines = input.lines();
+    let mut manifold: Vec<Vec<char>> = load_manifold("puzzle_input.txt");
 
-    let mut line = lines.next();
     let mut splits: usize = 0;
-    let mut beams: Vec<usize> = Vec::new();
-    let mut diagram: Vec<Vec<char>> = Vec::new();
-    loop {
-        if line.is_none() {
-            break;
-        };
-        let mut row_chars = line.unwrap().chars().collect::<Vec<char>>();
+    let mut beams: Vec<usize> = vec![];
 
+    for manifold_layer in manifold.iter_mut() {
+        // Insert the beams into the current layer
         for beam in beams {
-            (splits, row_chars) = insert_beam(beam, splits, row_chars);
+            insert_beam(beam, &mut splits, manifold_layer);
         }
 
-        beams = row_chars
+        // Find the positions of the beams in the current layer, including the initial 'S' beam
+        beams = manifold_layer
             .iter()
             .enumerate()
             .filter(|item| ['S', '|'].contains(item.1))
             .map(|item| item.0)
-            .collect();
-
-        diagram.push(row_chars);
-
-        line = lines.next();
+            .collect::<Vec<usize>>();
     }
 
     println!("Diagram:");
-    for r in &diagram {
+    for r in &manifold {
         let row: String = r.iter().collect();
         println!("{}", row);
     }
     println!("Splits: {splits}");
 
-    let initial_beam_position = diagram[0].iter().position(|x| *x == 'S').unwrap();
-    let timelines = count_timelines(initial_beam_position, &diagram, 0, diagram.len() - 1);
-
-    println!("Timelines {timelines}");
+    // Get number of timelines for part two of the problem
+    let mut extended_manifold = manifold.clone();
+    extended_manifold.push(vec!['.'; manifold[0].len()]);
+    let beam_positions: Vec<usize> = extended_manifold[0]
+        .iter()
+        .enumerate()
+        .filter(|item| ['S', '|'].contains(item.1))
+        .map(|item| item.0)
+        .collect();
+    let mut memo: Vec<Vec<Option<usize>>> = (0..extended_manifold.len())
+        .map(|i| vec![None; extended_manifold[i].len()])
+        .collect();
+    let mut sum: usize = 0;
+    for &beam_position in &beam_positions {
+        sum += count_timelines(beam_position, 0, &extended_manifold, &mut memo);
+    }
+    println!("Timelines: {sum}");
 }
 
-fn count_timelines(position: usize, diagram: &Vec<Vec<char>>, depth: usize, max_depth: usize) -> usize {
-    if depth >= max_depth {
-        return 1;
+fn count_timelines(
+    column: usize,
+    row: usize,
+    manifold: &Vec<Vec<char>>,
+    memo: &mut Vec<Vec<Option<usize>>>,
+) -> usize {
+    if row == manifold.len() {
+        return 1; // one completed timeline (leaf)
+    }
+
+    if let Some(v) = memo[row][column] {
+        return v;
+    }
+
+    let result = if manifold[row][column] == '^' {
+        let mut sum = 0;
+        if column > 0 {
+            sum += count_timelines(column - 1, row + 1, manifold, memo)
+        };
+        if column < (manifold[row].len() - 1) {
+            sum += count_timelines(column + 1, row + 1, manifold, memo)
+        };
+        sum
+    } else {
+        count_timelines(column, row + 1, manifold, memo)
     };
 
-    let row_chars = &diagram[depth];
-    let mut timelines = 0;
-    if row_chars[position] == '^' {
-        if position > 0 {
-            timelines += count_timelines(position - 1, diagram, depth + 1, max_depth);
-        };
-        if position < row_chars.len() - 2 {
-            timelines += count_timelines(position + 1, diagram, depth + 1, max_depth);
-        };
-    } else {
-        timelines = count_timelines(position, diagram, depth + 1, max_depth);
-    }
-    timelines
+    memo[row][column] = Some(result);
+
+    result
 }
 
-fn insert_beam(position: usize, mut splits: usize, mut row_chars: Vec<char>) -> (usize, Vec<char>) {
+fn load_manifold(file_path: &str) -> Vec<Vec<char>> {
+    let input = fs::read_to_string(file_path).unwrap();
+
+    let lines = input.lines();
+
+    lines
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect()
+}
+
+// If the position has:
+//  - empty space: insert a beam at the current position
+//  - a splitter: split the beam to the adjacent (left and right) positions
+fn insert_beam(position: usize, splits: &mut usize, row_chars: &mut Vec<char>) {
     match row_chars[position] {
         '.' => row_chars[position] = '|',
         '^' => {
-            splits += 1;
+            *splits += 1;
             if position > 0 {
-                (splits, row_chars) = insert_beam(position - 1, splits, row_chars)
+                insert_beam(position - 1, splits, row_chars)
             };
             if position < row_chars.len() - 1 {
-                (splits, row_chars) = insert_beam(position + 1, splits, row_chars)
+                insert_beam(position + 1, splits, row_chars)
             };
         }
         _ => {}
     }
-
-    (splits, row_chars)
 }
-
-// fn count_timelines(line: usize, position: usize, diagram: &Vec<Vec<char>>, path: &Vec<usize>, mut path_set: &mut BTreeSet<String>) -> usize {
-//     let mut new_timelines: usize = 0;
-//     let mut new_path = path.clone();
-//     let c = diagram[line][position];
-//     if ['|', 'S'].contains(&c) {
-//         new_path.push(position);
-//         if line == diagram.len() - 1 {
-//             let path_str: String = new_path.iter().map(|x|x.to_string()).collect::<Vec<_>>().join(" ");
-//             println!("Path: {}", path_str);
-//             if path_set.contains(&path_str) {println!("Found duplicate!: {:?}", new_path)}
-//             path_set.insert(path_str);
-//             return 1;
-//         }
-//         if position > 0 { new_timelines += count_timelines(line + 1, position - 1, diagram, &new_path, &mut path_set) };
-//         new_timelines += count_timelines(line + 1, position, diagram, &new_path, &mut path_set);
-//         if position < diagram[line].len() - 2 { new_timelines += count_timelines(line + 1, position + 1, diagram, &new_path, &mut path_set) };
-//     };
-//
-//     new_timelines
-// }
--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct JunctionBox {
+    x: isize,
+    y: isize,
+    z: isize
+}
+fn vector_distance(j1: JunctionBox, j2: JunctionBox) -> f32 {
+    let x2 = (j1.x - j2.x).pow(2);
+    let y2 = (j1.y - j2.y).pow(2);
+    let z2 = (j1.z - j2.z).pow(2);
+
+    let sq_distance = (x2 + y2 + z2) as f32;
+
+    sq_distance.sqrt()
+}
+
+#[cfg(test)]
+mod distance_tests {
+    use super::*;
+
+    #[test]
+    fn it_computes_distance_for_2d() {
+        let j1 = JunctionBox{x: 1, y: 1, z: 0};
+        let j2 = JunctionBox{x: 2, y: 2, z: 0};
+        assert_eq!(vector_distance(j1, j2), 2f32.sqrt());
+    }
+}
+
+
